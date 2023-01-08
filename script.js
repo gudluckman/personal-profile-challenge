3a3832c9c1f175fcfd417ccbd037312110f12686
Edward Lukman <103193885+gudluckman@users.noreply.github.com>
2023-01-08 20:53:09 +1100

    First Init

---
+++ script.js
@@ -0,0 +1,57 @@
+let i = 0;
+const intro = "I am Edward Lukman";
+let speed = 50;
+function typeWriter() {
+    if (i < intro.length) {
+      document.getElementById("header").innerHTML += intro.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
